b31ec85ad42f7d404e4c84142fa7ae872b6a642b
Stovakx <112606493+Stovakx@users.noreply.github.com>
2023-09-26 18:37:05 +0200

    Problem with img url
    more "s" then it should be

--- frontend/src/pages/homePage/IndexPage.jsx
+++ frontend/src/pages/homePage/IndexPage.jsx
@@ -1,11 +1,36 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
 import Navbar from "../navbar/Navbar";
-
+import UserWidget from "../widgets/UserWidget";
+import { useSelector } from "react-redux";
 
 export default function IndexPage() {
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const {_id, picturePath} = useSelector((state)=> state.user);
+
+
   return (
     <Box>
       <Navbar/>
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath}/>
+        </Box>
+        <Box flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined :"2rem"}
+        >
+          
+        </Box>
+        {isNonMobileScreens && (
+          <Box flexBasis="26%">
+              
+          </Box>
+        )}
+      </Box>
     </Box>
   )
 }
