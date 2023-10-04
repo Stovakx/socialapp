1816d52e477371b9c9d89fa675530f83ee0a7ddd
Stovakx <112606493+Stovakx@users.noreply.github.com>
2023-10-04 15:31:46 +0200

    cors

--- backend/server.js
+++ backend/server.js
@@ -25,13 +25,13 @@ const corsOptions = {
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials:true,
 };
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extendend: true }));
 app.use(bodyParser.urlencoded({ limit: "3Omb", extended: true }));
-app.use(cors(corsOptions));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 //file storage
