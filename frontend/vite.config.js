a827ab356cbaf1f61ec501634a4028b44b464df4
Stovakx <112606493+Stovakx@users.noreply.github.com>
2023-10-04 12:52:30 +0200

    Vite update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { baseUrl } from './src/utils/serverUrl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
     port:3000,
     host: "0.0.0.0",
+    proxy:{
+      baseUrl
+    }
   },
   plugins: [react()],
+  define:{
+    baseUrl
+  }
 })
