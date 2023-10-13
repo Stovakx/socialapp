dbd007f1a1fcce5d0c5603f478c0896c1da790bb
Stovakx <112606493+Stovakx@users.noreply.github.com>
2023-10-13 13:31:53 +0200

    vite confing host delete

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server:{
     port:3000,
-    host: "0.0.0.0",
+    /* host: "0.0.0.0", */
   },
   plugins: [react()],
 })
